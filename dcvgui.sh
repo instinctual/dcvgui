6fb7de010daba936578fa9ee57e542d7de512995
Gary Oberbrunner <garyo@darkstarsystems.com>
2024-05-03 17:45:50 -0400

    Add error dialog for errors; make some fields required
    Fixes #4

--- dcvgui.sh
+++ dcvgui.sh
@@ -17,6 +17,15 @@ check_prereqs () {
     mkdir -p "$CONFIGDIR"
 }
 
+show_error () {
+    local ERR_OPTIONS=(--text-info
+                       --height 100 --width 300 --title "Error"
+                       --fore=RED --margins=10 --wrap
+                       --button OK:0
+                      )
+    yad "${ERR_OPTIONS[@]}" <<< "Error: $*"
+}
+
 run_gui () {
     local GUI_OPTIONS=(--height 300 --title "Choose DCV Connection"
                        --no-click --ellipsize end
@@ -26,16 +35,21 @@ run_gui () {
                       )
     # List all filenames in config dir, without extension
     local NAMES
-    NAMES=$( (cd "$CONFIGDIR" && /bin/ls -1 -- *.dcv) | sed 's/\.dcv$//')
+    NAMES=$( (cd "$CONFIGDIR" && /bin/ls -1 -- *.dcv 2>/dev/null) | sed 's/\.dcv$//')
 
     local result
     result=$(echo "$NAMES" | yad --list "${GUI_OPTIONS[@]}" --column=Machine --separator "")
     local status=$?
     case $status in
         0 ) # OK
-            echo Running "$DCVVIEWER" "${DCVVIEWER_OPTIONS[@]}" "${CONFIGDIR}/${result}.dcv"
-            "$DCVVIEWER" "${DCVVIEWER_OPTIONS[@]}" "${CONFIGDIR}/${result}.dcv"
-            exit 0
+            if [[ -z $result ]]; then
+                show_error "Must choose a connection"
+                run_gui
+            else
+                echo Running "$DCVVIEWER" "${DCVVIEWER_OPTIONS[@]}" "${CONFIGDIR}/${result}.dcv"
+                "$DCVVIEWER" "${DCVVIEWER_OPTIONS[@]}" "${CONFIGDIR}/${result}.dcv"
+                exit 0
+            fi
             ;;
         1 ) # Cancel
             exit 1
@@ -47,13 +61,12 @@ run_gui () {
 }
 
 run_create () {
-    echo "Running Create Connection GUI"
     local CREATE_OPTIONS=(--height 500 --title "Create DCV Connection"
                           --text "Connection Details:"
                           --field "Connection Name" ""
                           --field "Host Name/IP" ""
                           --field Port:NUM "$DEFAULT_PORT"
-                          --field User
+                          --field User ""
                          )
     # Show the Create GUI form
     result=$(yad --form "${CREATE_OPTIONS[@]}")
@@ -63,12 +76,15 @@ run_create () {
         run_gui
     fi
 
-    echo "$result"
     IFS="|" read -r connection_name ip port user <<< "$result"
     connection_name="${connection_name//\//_}" # remove slashes
+    if [[ -z $connection_name || -z $ip || -z $port ]]; then
+        show_error "Connection name, IP addr and port must all be specified."
+        run_gui
+    fi
     local fname="${CONFIGDIR}/${connection_name}.dcv"
     if [[ -e "$fname" ]]; then
-        echo "Connection $fname already exists; please remove it or use a different name"
+        show_error "Connection $fname already exists; please remove it or use a different name"
         exit 1
     fi
     # Create the file
