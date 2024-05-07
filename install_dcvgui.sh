7825662f991ee278dbcaf4b5da9a5eb9f3ff65ff
Alan Latteri <alan@instinctual.la>
2024-05-06 19:49:03 -0700

    add install script.  remove sample .dcv file

---
+++ install_dcvgui.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Check if the current user ID is not 0 (root has a UID of 0)
+if [[ $(id -u) -ne 0 ]]; then
+    echo "This script must be run as root or with sudo."
+    exit 1
+fi
+
+mkdir -p /opt/instinctual/bin/
+/usr/bin/install -m 655 dcvgui.sh /opt/instinctual/bin/
+
+#install shortcut
+/usr/bin/install -m 644 dcvgui.desktop /usr/share/applications/
+/usr/bin/install -m 644 dcvgui.png /opt/instinctual
+ln -sf /usr/share/applications/dcvgui.desktop /etc/xdg/autostart/
